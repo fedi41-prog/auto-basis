07fc8753280ea680a4053588fe1afeaac92571a2
Fedor <crocofedi4@gmail.com>
2025-01-13 17:42:10 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+radio.onReceivedValue(function (name, value) {
+    if (verbunden == 0) {
+    	
+    } else if (false) {
+    	
+    }
+})
+let verbunden = 0
+verbunden = 0
+let code = 4512652
+radio.setGroup(128)
+radio.setTransmitSerialNumber(true)
